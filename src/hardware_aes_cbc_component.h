6f5544f15c4ef6e1f6c63f0f847965f1df7f5631
cybermatte <98705084+cybermatte@users.noreply.github.com>
2025-08-07 21:53:54 +0200

    Update hardware_aes_cbc_component.h

--- src/hardware_aes_cbc_component.h
+++ src/hardware_aes_cbc_component.h
@@ -45,3 +45,17 @@ class HardwareAESCBCComponent : public esphome::Component {
     0x10, 0x32, 0x54, 0x76, 0x98, 0xBA, 0xDC, 0xFE
   };
 };
+
+uint16_t crc16(const uint8_t *data, size_t len) {
+  uint16_t crc = 0xFFFF;
+  for (size_t i = 0; i < len; ++i) {
+    crc ^= data[i];
+    for (int j = 0; j < 8; ++j) {
+      if (crc & 1)
+        crc = (crc >> 1) ^ 0xA001;
+      else
+        crc >>= 1;
+    }
+  }
+  return crc;
+}
