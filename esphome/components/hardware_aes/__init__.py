8e2440cffcd702592c8c8731a135656c3ec41683
cybermatte <98705084+cybermatte@users.noreply.github.com>
2025-08-12 19:56:18 +0200

    Update __init__.py

--- esphome/components/hardware_aes/__init__.py
+++ esphome/components/hardware_aes/__init__.py
@@ -1,1 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+hardware_aes_ns = cg.new_ns("hardware_aes")
+HardwareAESComponent = hardware_aes_ns.class_("HardwareAESComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(HardwareAESComponent),
+    cv.Required("key"): cv.string_strict,
+    cv.Required("hmac_key"): cv.string_strict,
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
+
+    cg.add(var.set_key(config["key"]))
+    cg.add(var.set_hmac_key(config["hmac_key"]))
+
 
