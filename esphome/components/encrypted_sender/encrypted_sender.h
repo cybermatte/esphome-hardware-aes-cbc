f9364f6abf4e5cc9f8747c29594e5ed86449aaa0
cybermatte <98705084+cybermatte@users.noreply.github.com>
2025-08-12 20:40:08 +0200

    Update encrypted_sender.h

--- esphome/components/encrypted_sender/encrypted_sender.h
+++ esphome/components/encrypted_sender/encrypted_sender.h
@@ -3,7 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/sx126x/sx126x.h"
-#include "esphome/components/hardware_aes/hardware_aes_cbc.h"
+#include "esphome/components/hardware_aes/hardware_aes.h"
 
 namespace esphome {
 namespace encrypted_sender {
@@ -12,7 +12,8 @@ class EncryptedSenderComponent : public Component {
  public:
   void set_temperature_sensor(sensor::Sensor *sensor) { temperature_sensor_ = sensor; }
   void set_humidity_sensor(sensor::Sensor *sensor) { humidity_sensor_ = sensor; }
-  void set_aes(hardware_aes::HardwareAESCBCComponent *aes) { aes_ = aes; }
+  void set_aes(hardware_aes::HardwareAESComponent *aes);
+
   void set_lora(sx126x::SX126x *lora) { lora_ = lora; }
   void set_update_interval(uint32_t ms) { update_interval_ms_ = ms; }
 
@@ -22,7 +23,8 @@ class EncryptedSenderComponent : public Component {
  protected:
   sensor::Sensor *temperature_sensor_{nullptr};
   sensor::Sensor *humidity_sensor_{nullptr};
-  hardware_aes::HardwareAESCBCComponent *aes_{nullptr};
+  hardware_aes::HardwareAESComponent *aes_{nullptr};
+
   sx126x::SX126x *lora_{nullptr};
 
   uint32_t update_interval_ms_{60000};  // default to 60s
