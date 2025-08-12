3a551bcf7f61f09af6646b93fdc036c10bf624af
cybermatte <98705084+cybermatte@users.noreply.github.com>
2025-08-12 20:23:15 +0200

    Update encrypted_sender.h

--- esphome/components/encrypted_sender/encrypted_sender.h
+++ esphome/components/encrypted_sender/encrypted_sender.h
@@ -1,26 +1,36 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/lora/lora.h"
-#include "esphome/components/hardware_aes/hardware_aes.h"
+#include "esphome/components/sx126x/sx126x.h"
+#include "esphome/components/hardware_aes/hardware_aes_cbc.h"
 
 namespace esphome {
 namespace encrypted_sender {
 
 class EncryptedSenderComponent : public Component {
  public:
-  sensor::Sensor *temperature_sensor;
-  sensor::Sensor *humidity_sensor;
-  hardware_aes::HardwareAESComponent *aes;
-  lora::LoraComponent *lora;
+  void set_temperature_sensor(sensor::Sensor *sensor) { temperature_sensor_ = sensor; }
+  void set_humidity_sensor(sensor::Sensor *sensor) { humidity_sensor_ = sensor; }
+  void set_aes(hardware_aes::HardwareAESCBCComponent *aes) { aes_ = aes; }
+  void set_lora(sx126x::SX126x *lora) { lora_ = lora; }
+  void set_update_interval(uint32_t ms) { update_interval_ms_ = ms; }
 
-  void set_temperature_sensor(sensor::Sensor *sensor) { temperature_sensor = sensor; }
-  void set_humidity_sensor(sensor::Sensor *sensor) { humidity_sensor = sensor; }
-  void set_aes(hardware_aes::HardwareAESComponent *comp) { aes = comp; }
-  void set_lora(lora::LoraComponent *comp) { lora = comp; }
+  void setup() override;
+  void loop() override;
 
-  void update() override;
+ protected:
+  sensor::Sensor *temperature_sensor_{nullptr};
+  sensor::Sensor *humidity_sensor_{nullptr};
+  hardware_aes::HardwareAESCBCComponent *aes_{nullptr};
+  sx126x::SX126x *lora_{nullptr};
+
+  uint32_t update_interval_ms_{60000};  // default to 60s
+  uint32_t last_send_{0};
+
+  void send_encrypted_data_();
 };
 
 }  // namespace encrypted_sender
 }  // namespace esphome
+
